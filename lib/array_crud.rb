097ba9a02a0e4d7875996fb0607eaa499cd8aa4c
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 02:40:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  ["dog", "cat", "bird", "turtle"][-1]
+  ["dog", "cat", "bird", "wow"][-1]
 end
 
 def retrieve_last_element_from_array(array)
