f43908606cd96d12a27907dbd8a4517d385965fa
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 02:38:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  .shift
+  ["dog", "cat", "bird", "turtle"].shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  [dog, cat, bird, turtle][1]
+  [1]
 end
 
 def retrieve_first_element_from_array(array)
